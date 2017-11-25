6f8bcfb29039b7122906789d35977f42476f0c0c
darke333 <33874124+darke333@users.noreply.github.com>
2017-11-25 14:11:01 +0300

    авива

---
+++ Lab3/Dot.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class Dot
+    {
+        int x;
+        int y;
+        bool type;
+        int speed;
+        int size;
+        int fill;
+
+        public Dot(int X, int Y, bool Type)
+        {
+            type = Type;
+            x = X;
+            y = Y;
+            size = 200;
+            speed = size / 10;
+            fill = size / 2;
+        }
+
+        public Dot(bool Type)
+        {
+            type = Type;
+            size = 200;
+            speed = size / 10;
+            fill = size / 2;
+        }
+
+        public Dot(int X, int Y)
+        {
+            x = X;
+            y = Y;
+            size = 200;
+            speed = size / 10;
+            fill = size / 2;
+        }
+
+
+
+    }
+}
